9f719dd3fd0387b397ccb3b6f409977b56d6c83e
idanless <31201417+idanless@users.noreply.github.com>
2020-03-27 08:29:17 +0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -11,17 +11,23 @@ class BankAccount:
         self.balance -= amount
 
     def print_out(self,name):
-        print("your balace is:" , name, self.balance)
+        print("your balance is:" , name, self.balance)
 
+class Bank(BankAccount):
+    def __int__(self,name1='y'):
+        BankAccount.__init__(self,balance=0)
+        self.name1 = name1
 
-
-
+    def print_y(self, name1):
+        print("your balance is:", name1,self.balance,self.bank_name)
 
 idan = BankAccount()
+idanname = Bank()
+idanname.deposit(150)
+idanname.withdraw(20)
+idanname.print_y('idan')
 idan.deposit(100)
 idan.withdraw(50)
-
-print("the bank is :" ,idan.bank_name)
-print( idan.print_out('idan'))
-
-
+idan.bank_name = 'idan'
+x = idan.balance
+#print(str(x)+'\t'+str(idan.bank_name))
